3c371cf75c9692c3aede1bea935ec6c68e44daa3
John Doe <johndoe@example.com>
2015-12-13 13:29:10 +0800

    optimize: Expression Add Operators

--- algorithms/expression_add_operators.rb
+++ algorithms/expression_add_operators.rb
@@ -24,78 +24,32 @@
 def add_operators(num, target)
   return [] if num.empty?
 
-  split = Array(0...num.size).each_with_object({}) do |spt, memo|
-    subnum = num.slice(spt...num.size)
-    memo[subnum] =
-      if subnum[0] == '0'
-        [[0, subnum.slice(1..-1)]]
-      else
-        Array(1..subnum.size).map do |subspt|
-          [subnum.slice(0...subspt).to_i, subnum.slice(subspt..-1)]
-        end
-      end
+  result = []
+  1.upto(num.size) do |spt|
+    break if spt - 0 > 1 && num[0] == '0'
+
+    n = num[0...spt].to_i
+    _add_operators_(num, spt, target, n, "#{n}", '#', n, result)
   end
-
-  rnumbers = Array(1..num.size).map { |spt| num[-spt..-1] }
-    rcache = rnumbers.each_with_object({}) { |n, memo| memo[n] = [] }
-
-  out = []
-  rnumbers.each do |number|
-    q, c = [[[nil], number, '']], rcache[number]
-
-    until q.empty?
-      deque, istream, ostream = q.shift
-
-      split[istream].each do |lnum, rnum|
-        if rnum.empty?
-          deque.push(lnum); _calculate_(deque, :'$')
-          if number == num
-            out << "#{ostream}#{lnum}" if deque[-1] == target
-          else
-            c << [deque[-1], "#{ostream}#{lnum}"]
-          end; next
-        end
-
-        rcache[rnum].each do |v, e|
-          de = deque.dup; de.push(lnum); _calculate_(de, :+)
-          de.push(v); _calculate_(de, :'$')
-          if number == num
-            out << "#{ostream}#{lnum}+#{e}" if de[-1] == target
-          else
-            c << [de[-1], "#{ostream}#{lnum}+#{e}"]
-          end
-        end
-
-        de = deque.dup; de.push(lnum); _calculate_(de, :-)
-        q << [de, rnum, "#{ostream}#{lnum}-"]
-
-        de = deque.dup; de.push(lnum); _calculate_(de, :*)
-        q << [de, rnum, "#{ostream}#{lnum}*"]
-      end
-    end
-  end
-
-  out
+  result
 end
 
-private def _calculate_(deque, next_op)
-  case next_op
-  when :+, :-, :'$'
-    while true
-      case deque[0]
-      when nil; break
-      when :+; deque.push(deque.pop + deque.pop)
-      when :-; deque.push(- deque.pop + deque.pop)
-      when :*; deque.push(deque.pop * deque.pop)
-      end
-      deque.shift
-    end
-    deque.unshift(next_op)
-  when :*
-    if deque[0] == :*
-      deque.shift
-      deque.push(deque.pop * deque.pop)
-    end
-    deque.unshift(next_op)
+private def _add_operators_(num, idx, target, value, expr, op, multiplier, result)
+  if idx == num.size
+    result << expr if value == target
+    return
+  end
+
+  Range.new(idx + 1, num.size).each do |spt|
+    break if spt - idx > 1 && num[idx] == '0'
+
+    n = num[idx...spt].to_i
+    _add_operators_(num, spt, target, value + n, "#{expr}+#{n}", '+', n, result)
+    _add_operators_(num, spt, target, value - n, "#{expr}-#{n}", '-', n, result)
+
+    v = value * n
+    v = value - multiplier + multiplier * n if op == '+'
+    v = value + multiplier - multiplier * n if op == '-'
+    _add_operators_(num, spt, target, v, "#{expr}*#{n}", op, multiplier * n, result)
   end
 end
