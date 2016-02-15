e3fb774a357a25380e1ff53cf423a732c790bd54
John Doe <johndoe@example.com>
2016-02-15 20:35:54 +0800

    style: comment

--- database/combine_two_tables.sql
+++ database/combine_two_tables.sql
@@ -2,28 +2,24 @@
 --
 -- Table: Person
 --
---    +-------------+---------+
---    | Column Name | Type    |
---    +-------------+---------+
---    | PersonId    | int     |
---    | FirstName   | varchar |
---    | LastName    | varchar |
---    +-------------+---------+
---
---    PersonId is the primary key column for this table.
+--     +-------------+---------+
+--     | Column Name | Type    |
+--     +-------------+---------+
+--     | PersonId    | int     |
+--     | FirstName   | varchar |
+--     | LastName    | varchar |
+--     +-------------+---------+
 --
 -- Table: Address
 --
---    +-------------+---------+
---    | Column Name | Type    |
---    +-------------+---------+
---    | AddressId   | int     |
---    | PersonId    | int     |
---    | City        | varchar |
---    | State       | varchar |
---    +-------------+---------+
---
---    AddressId is the primary key column for this table.
+--     +-------------+---------+
+--     | Column Name | Type    |
+--     +-------------+---------+
+--     | AddressId   | int     |
+--     | PersonId    | int     |
+--     | City        | varchar |
+--     | State       | varchar |
+--     +-------------+---------+
 --
 -- Write a SQL query for a report that provides the following information for
 -- each person in the Person table, regardless if there is an address for each
