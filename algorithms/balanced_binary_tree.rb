3f21cba32cfd00d980419a86cb0de74938fcc7a8
John Doe <johndoe@example.com>
2016-03-18 21:17:38 +0800

    style: comment

--- algorithms/balanced_binary_tree.rb
+++ algorithms/balanced_binary_tree.rb
@@ -8,10 +8,10 @@
 # Definition for a binary tree node.
 # class TreeNode
 #   attr_accessor :val, :left, :right
-#     def initialize(val)
-#       @val = val
-#       @left, @right = nil, nil
-#     end
+#   def initialize(val)
+#     @val = val
+#     @left, @right = nil, nil
+#   end
 # end
 
 
